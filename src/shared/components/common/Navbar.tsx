e406da56aa4eea71bd1c00b8993db081be14d3da
supamekin <supamekin@gmail.com>
2024-01-24 12:33:00 +0700

    Edit Nanber

--- src/shared/components/common/Navbar.tsx
+++ src/shared/components/common/Navbar.tsx
@@ -13,10 +13,10 @@ const Navbar = () => {
         <img src={logo_profile_W} className=""/>
       </div>
       <nav className=" flex items-center font-bold">
-        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >หน้าแรก</div>
-        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >ผลงาน</div>
-        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >เกี่ยวกับเรา</div>
-        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >บทความ</div>
+        <div className=" mr-7 text-base text-white cursor-pointer" >หน้าแรก</div>
+        <div className=" mr-7 text-base text-white cursor-pointer" >ผลงาน</div>
+        <div className=" mr-7 text-base text-white cursor-pointer" >เกี่ยวกับเรา</div>
+        <div className=" mr-7 text-base text-white cursor-pointer" >บทความ</div>
         <div onClick={() => routeTo("/userlogin")} className=" w-[70px] bg-white text-center rounded m-2 p-1 cursor-pointer">Sign in</div>
         <div onClick={() => routeTo("/register")} className=" w-[70px] bg-blue-500 text-center rounded m-2 p-1 text-white cursor-pointer" >Sign up</div>
       </nav>
