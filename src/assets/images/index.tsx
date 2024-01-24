f90289b5e70a6116e6dcc36d8c25411e8317b875
supamekin <supamekin@gmail.com>
2024-01-24 11:49:33 +0700

    my first commit

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,6 +1,7 @@
 import bgTable from "./bgTable.webp"
 import bgCafe02 from "./bgcafe0_2.webp"
 import logo_profile from "./Logo_Profile_pages-to-jpg-0001.jpg"
+import logo_profile_W from "./Logo_Profile_W.png"
 
 
-export {bgTable, bgCafe02, logo_profile}
+export {bgTable, bgCafe02, logo_profile, logo_profile_W}
--- src/shared/components/common/Navbar.tsx
+++ src/shared/components/common/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { logo_profile } from "../../../assets/images"
+import { logo_profile_W } from "../../../assets/images"
 const Navbar = () => {
   const navigate = useNavigate();
   const routeTo = (path) => {
@@ -7,12 +7,20 @@ const Navbar = () => {
   };
 
   return (
-    <header className=" bg-gray-700 p-2 flex ">
-      <div className=" " style={{ backgroundImage: `url(${logo_profile})`}}>ert</div>
-      <nav className=" flex justify-end">
-        <div onClick={() => routeTo("/userlogin")} className=" w-[70px] bg-white text-center rounded m-2 p-1 font-bold cursor-pointer">Sign in</div>
-        <div onClick={() => routeTo("/register")} className=" w-[70px] bg-blue-500 text-center rounded m-2 p-1 text-white font-bold cursor-pointer" >Sign up</div>
+    <header className=" bg-[#333] p-2 ">
+      <div className=" flex items-center justify-between max-w-[1140px] m-auto ">
+      <div className="flex items-center w-[180px] h-[30px] " >
+        <img src={logo_profile_W} className=""/>
+      </div>
+      <nav className=" flex items-center font-bold">
+        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >หน้าแรก</div>
+        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >ผลงาน</div>
+        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >เกี่ยวกับเรา</div>
+        <div onClick={() => routeTo("/register")} className=" mr-7 text-base text-white cursor-pointer" >บทความ</div>
+        <div onClick={() => routeTo("/userlogin")} className=" w-[70px] bg-white text-center rounded m-2 p-1 cursor-pointer">Sign in</div>
+        <div onClick={() => routeTo("/register")} className=" w-[70px] bg-blue-500 text-center rounded m-2 p-1 text-white cursor-pointer" >Sign up</div>
       </nav>
+      </div>
     </header>
   );
 };
